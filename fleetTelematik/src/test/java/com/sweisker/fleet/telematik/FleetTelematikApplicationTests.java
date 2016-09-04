b8deda13fbb6b2c9e8e0c576d29e0c04ef2c597d
Sven Weisker <sweisker@web.de>
2016-09-04 12:56:09 +0200

    integrated osm2po

--- fleetTelematik/src/test/java/com/sweisker/fleet/telematik/FleetTelematikApplicationTests.java
+++ fleetTelematik/src/test/java/com/sweisker/fleet/telematik/FleetTelematikApplicationTests.java
@@ -1,16 +1,22 @@
 package com.sweisker.fleet.telematik;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import java.io.FileNotFoundException;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class FleetTelematikApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void loadRouteFile() throws FileNotFoundException{
+		FleetTelematikApplication.loadRouteStream();
+		assertTrue(true);
 	}
 
 }
